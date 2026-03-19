0610f9d500500aeb45e562751af622bf4ff88674
Regina Annemarie Bool <ninisalter@gmail.com>
2026-03-19 08:18:31 +0800

    Initial Commit

---
+++ N-th Fibonacci number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std; // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610
+
+int main(){
+    int n;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    if (n < 0) { cout << "Invalid input"; return 0; } // handles negative numbers
+    
+    if (n == 0) { cout << "0-th Fibonacci number is 0"; return 0;} // edge case with 0
+
+    int prev = 0;
+    int curr = 1;
+    int next = 1;
+
+    for (int i = 2; i < n; i++) { // iterative summation process
+        next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+
+    cout << n << "-th Fibonacci number is " << curr << endl;
+
+    return 0;
+}
